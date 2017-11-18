db089e3a4a4c451fef230ae56fccb65e59c6b38d
Tim White <tim@cyface.com>
2017-11-18 11:15:06 -0700

    compose script updates

--- devscripts/docker-compose/restart.sh
+++ devscripts/docker-compose/restart.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 eval "$(docker-machine env default)"
-docker-compose stop nginx
-docker-compose stop interfaceserver
-docker-compose stop workerserver_1
-docker-compose stop workerserver_2
-docker-compose stop db redis
+docker-compose stop
 sleep 1s
 docker-compose up -d
--- devscripts/docker-compose/stop.sh
+++ devscripts/docker-compose/stop.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 eval "$(docker-machine env default)"
-docker-compose stop interfaceserver
-docker-compose stop workerserver_1
-docker-compose stop workerserver_2
-docker-compose stop db redis
+docker-compose down
