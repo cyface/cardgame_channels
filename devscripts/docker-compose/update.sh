e8d66b56cc4c7897566fbf84befebfec47d7cc27
Tim White <tim@cyface.com>
2017-11-18 21:56:00 -0700

    compose script updates

--- devscripts/docker-compose/update.sh
+++ devscripts/docker-compose/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+eval "$(docker-machine env default)"
 git pull
-docker-compose build
-docker-compose up -d --force-recreate
+docker-compose up -d --force-recreate --build
 docker-compose run --rm interfaceserver python manage.py migrate --noinput
 docker-compose run --rm interfaceserver python manage.py collectstatic --noinput
