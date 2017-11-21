2932f5e3e3c3f94a41a0493e9f6db9f9709ddd7e
Tim White <cyface@dev.cyface.com>
2017-11-20 22:05:49 -0700

    update tweak

--- devscripts/docker-compose/update.sh
+++ devscripts/docker-compose/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git pull
 docker-compose up -d --force-recreate --build
+sleep 15
 docker-compose run --rm interfaceserver python manage.py migrate --noinput
 docker-compose run --rm interfaceserver python manage.py collectstatic --noinput
