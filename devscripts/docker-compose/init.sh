458abcdb7e4ce61ed9090ecab9b091d4a4df1aba
Tim White <tim@cyface.com>
2017-11-20 17:12:32 -0700

    compose script updates

--- devscripts/docker-compose/init.sh
+++ devscripts/docker-compose/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker-compose run interfaceserver python manage.py migrate --noinput
-docker-compose run interfaceserver python manage.py loaddata initial --noinput
+docker-compose run interfaceserver python manage.py loaddata initial
 docker-compose run interfaceserver python manage.py collectstatic --noinput
 docker-compose run interfaceserver python manage.py createsuperuser
