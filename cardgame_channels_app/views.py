615a25a090d944e2dd51d5916a4d60fdd8279ed7
cyface <tim@cyface.com>
2017-12-18 08:36:37 -0700

    cleanup imports

--- cardgame_channels_app/views.py
+++ cardgame_channels_app/views.py
@@ -1,7 +1,5 @@
 import logging
 
-from django.conf import settings
-from django.core.mail import send_mail
 from django.views.generic import TemplateView
 
 LOGGER = logging.getLogger("cardgame_channels_app")
